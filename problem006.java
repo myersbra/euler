b985b389a0fbae4bd593bc073f2ce93346f78e60
brad <myers.1391@osu.edu>
2015-04-26 13:54:11 -0400

    java

---
+++ problem006.java
@@ -0,0 +1,24 @@
+// Sum Square Difference
+// 
+// The sum of the squares of the first ten natural numbers is:
+// 12 + 22 + ... + 102 = 385
+// 
+// The square of the sum of the first ten natural numbers is:
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// 
+// Hence the difference between the sum of the squares of the first
+// ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+// 
+// Find the difference between the sum of the squares of the first one
+// hundred natural numbers and the square of the sum.
+
+class problem006 {
+  public static void main(String[] args) {
+  int sum = 0, squares = 0;
+  for (int i = 1; i <= 100; i++) {
+    squares += i * i;
+    sum += i;
+  }
+  System.out.println((sum * sum) - squares);
+  }
+}
