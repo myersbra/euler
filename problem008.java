ba1437dc3600435f5e8ec200542ba31f688acd04
brad <myers.1391@osu.edu>
2015-04-26 18:16:41 -0400

    java

--- problem008.java
+++ problem008.java
@@ -7,34 +7,34 @@ import java.io.*;
 import java.util.*;
 
 class problem008 {
-	public static void main(String[] args) {
-		try {
-			// read file into Integer List
-			BufferedReader br = new BufferedReader(new FileReader("problem008.txt"));
-			List<Integer> intList = new ArrayList<>();
-			String line;
-			while ((line = br.readLine()) != null) {
-				for (int i = 0; i < line.length(); i++) {
-					intList.add(Character.getNumericValue(line.charAt(i)));
-				}
-			}
+  public static void main(String[] args) {
+    try {
+      // read file into Integer List
+      BufferedReader br = new BufferedReader(new FileReader("problem008.txt"));
+      List<Integer> intList = new ArrayList<>();
+      String line;
+      while ((line = br.readLine()) != null) {
+        for (int i = 0; i < line.length(); i++) {
+          intList.add(Character.getNumericValue(line.charAt(i)));
+        }
+      }
 
-			// check all consecutive 5-digit combinations
-			int largestProduct = 0;
-			for (int i = 0; i < intList.size() - 5; i++) {
-			  int product = intList.get(i);
-			  for (int j = i + 1; j < i + 5; j++) {
-			    product *= intList.get(j);
-			    if (product > largestProduct) largestProduct = product;
-			  }
-			}
-			System.out.println(largestProduct);
-		}
-		catch (FileNotFoundException e) {
-			System.out.println(e);
-		}
-		catch (IOException e) {
-			System.out.println(e);
-		}
-	}
+      // check all consecutive 5-digit combinations
+      int largestProduct = 0;
+      for (int i = 0; i < intList.size() - 5; i++) {
+        int product = intList.get(i);
+        for (int j = i + 1; j < i + 5; j++) {
+          product *= intList.get(j);
+          if (product > largestProduct) largestProduct = product;
+        }
+      }
+      System.out.println(largestProduct);
+    }
+    catch (FileNotFoundException e) {
+      System.out.println(e);
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }
+  }
 }
