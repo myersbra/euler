4e2718ee741ba6a711776518a25c9e4851785526
brad <myers.1391@osu.edu>
2015-04-26 19:17:59 -0400

    java

--- problem008.java
+++ problem008.java
@@ -9,24 +9,24 @@ import java.util.*;
 class problem008 {
   public static void main(String[] args) {
     try {
-      // read file into Integer List
+      // read filelongo Integer List
       BufferedReader br = new BufferedReader(new FileReader("problem008.txt"));
-      List<Integer> intList = new ArrayList<>();
+      List<Integer> list = new ArrayList<>();
       String line;
       while ((line = br.readLine()) != null) {
         for (int i = 0; i < line.length(); i++) {
-          intList.add(Character.getNumericValue(line.charAt(i)));
+         list.add(Character.getNumericValue(line.charAt(i)));
         }
       }
 
-      // check all consecutive 5-digit combinations
-      int largestProduct = 0;
-      for (int i = 0; i < intList.size() - 5; i++) {
-        int product = intList.get(i);
-        for (int j = i + 1; j < i + 5; j++) {
-          product *= intList.get(j);
-          if (product > largestProduct) largestProduct = product;
+      // check all consecutive 13-digit combinations
+     long product = 0, largestProduct = 0;
+      for (int i = 0; i < list.size() - 12; i++) {
+        product = list.get(i);
+        for (int j = 1; j < 13; j++) {
+          product *= list.get(i + j);
         }
+        if (product > largestProduct) largestProduct = product;
       }
       System.out.println(largestProduct);
     }
