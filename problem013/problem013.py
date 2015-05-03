1675c607d852c9a6ad6d516a8ab362e477db9ba5
brad <myers.1391@osu.edu>
2015-05-03 13:39:02 -0400

    13

---
+++ problem013/problem013.py
@@ -0,0 +1,11 @@
+# Large Sum
+# 
+# Work out the first ten digits of the sum of
+# the one-hundred 50-digit numbers in problem013.txt.
+
+total = 0
+with open('problem013.txt') as f:
+    for line in f:
+        total += int(line)
+
+print(str(total)[:10])
