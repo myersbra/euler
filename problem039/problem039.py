34623eee6b522724aa98f954417617aa17f96a4f
myersbra <myersbra@users.noreply.github.com>
2015-07-22 08:34:07 -0400

    Update problem039.py

--- problem039/problem039.py
+++ problem039/problem039.py
@@ -6,17 +6,20 @@
 # For which value of p <= 1000, is the number of solutions maximised?
 
 import math
+import operator
 from collections import defaultdict
 
-triplecounts = defaultdict()
+triplecounts = defaultdict(int)
 c = 0
 
 for a in range(1, 1000):
   for b in range(a, 1000):
     c = math.hypot(a, b)
-    if c % 1.0 != 0: next
+    if c % 1 != 0.0:
+      continue
     side_length_sum = a + b + int(c)
-    if (side_length_sum) > 1000: break
+    if side_length_sum > 1000:
+      break
     triplecounts[side_length_sum] += 1
 
-print max(triplecounts, key=triplecounts.get)
+print(max(triplecounts.iteritems(), key=operator.itemgetter(1))[0])
